9136d351beb4719c56a086b89a58dc5b9877dfca
Javier Vera Duran <jverad@MACJVERAD.local>
2024-12-10 14:11:07 -0300

    feat: Add ACT2.5

--- EA2/ACT2.5/README.md
+++ EA2/ACT2.5/README.md
@@ -7,4 +7,6 @@
 git tag -a v1.0.0 -m "Release version 1.0.0"
 git push origin v1.0.0
 
+[Github Release](https://docs.github.com/es/repositories/releasing-projects-on-github/managing-releases-in-a-repository)
+
 ## REFLEXIONES
--- EA2/ACT2.5/main.tf
+++ EA2/ACT2.5/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                = "./vpc_module"
+  source                = "github.com/Fundacion-Instituto-Profesional-Duoc-UC/AUY1105-Infraestructura-como-codigo-II//EA2/ACT2.5/vpc_module?ref=v1.0.0"
   vpc_cidr              = var.vpc_cidr
   vpc_name              = var.vpc_name
   subnet_publica_1_cidr = var.subnet_publica_1_cidr
@@ -11,7 +11,7 @@ module "vpc" {
 }
 
 module "ec2" {
-  source        = "./ec2_module"
+  source        = "github.com/Fundacion-Instituto-Profesional-Duoc-UC/AUY1105-Infraestructura-como-codigo-II//EA2/ACT2.5/ec2_module?ref=v1.0.0"
   key_name      = var.key_name
   public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDiuFUssdtHg8Y3rWGZFCSD58hSr4IqjFVKeid9d0G3bk7w99/AOyL/C45PnFodjOtD1eMndiCd40BqagdOYtKoieqlOTlmShrvE7N2A+MeaOP4CWLx7fj2MfekecPPFRAiMUCZk51SHxFr4oqX4Qhj8BkG1cG30p9QB+stfJKT3tUGczxUB1aor9qoLmPDTfaE4iSmNDscVmqQhX9jkppdzkg2ENh5cDO2EtLlHHxIodXLgetpWjBP68r90q/gwZV69XANcTWjZiZRyDmb9nIfQiZOO5C03FoG0GmTSZkAfvZdq7M2GsQSboln44VW/ukyQKFRVVepOCIHTaqcsjhV"
   ami           = "ami-012967cc5a8c9f891"
@@ -19,3 +19,5 @@ module "ec2" {
   vpc_id        = module.vpc.vpc_id
   instance_name = var.instance_name
 }
+
+
