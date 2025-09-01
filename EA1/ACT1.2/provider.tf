7df2c1c8607a346446764b2d45252a8c509b7fff
Nico Singh <4417761+nicosingh@users.noreply.github.com>
2025-09-01 19:31:29 -0400

    tflint fixes

--- EA1/ACT1.2/provider.tf
+++ EA1/ACT1.2/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 1.13.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.11.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
 }
