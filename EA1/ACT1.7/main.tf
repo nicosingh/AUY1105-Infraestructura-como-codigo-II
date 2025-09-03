78f4a3b3cfe0554d219fa99391f2b02778731c68
Nico Singh <4417761+nicosingh@users.noreply.github.com>
2025-09-03 18:48:25 -0400

    Refactor Terraform configurations across multiple activities to integrate Azure backend and modules
    - Updated .gitignore files to exclude tfplan files.
    - Removed outdated README.md files and replaced them with new content reflecting the latest activity requirements.
    - Deleted unused Terraform files (data.tf, ec2.tf, provider.tf, vpc.tf, install.sh) to streamline the project structure.
    - Added main.tf files for each activity, configuring Azure backend and modules for infrastructure deployment.
    - Ensured outputs for virtual networks, storage accounts, and virtual machine details are included in the new main.tf files.

---
+++ EA1/ACT1.7/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_version = "~> 1.13.0"
+
+  backend "azurerm" {
+    resource_group_name  = "AreaInfraestructura"
+    storage_account_name = "infracomocodigoduoc2025"
+    container_name       = "terraform-state"
+    key                  = "clase-4.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.41.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  resource_provider_registrations = "none"
+  subscription_id                 = "bb1ccac7-d7f8-47bf-82c2-f223185cfab9"
+}
+
+module "infraestructura_azure" {
+  source = "git::https://gitlab.com/nicosingh/curso-infra-como-codigo-ago-sep-2025.git//clase-4/modulo-interno-azure?ref=6125164b0ccb27ea2f53c950bbbdda3b539115db"
+
+  nombre_proyecto = "clase-4"
+  ubicacion       = "East US 2"
+  ambiente        = terraform.workspace
+
+  nombre_grupo_recursos = "AreaInfraestructura"
+
+  crear_cuenta_almacenamiento = true
+  crear_maquina_virtual       = true
+}
+
+output "nombre_red_virtual" {
+  value       = module.infraestructura_azure.nombre_red_virtual
+  description = "Nombre de la Red Virtual creada"
+}
+
+output "id_red_virtual" {
+  value       = module.infraestructura_azure.id_red_virtual
+  description = "ID de la Red Virtual creada"
+}
+
+output "nombre_cuenta_almacenamiento" {
+  value       = module.infraestructura_azure.nombre_cuenta_almacenamiento
+  description = "Nombre de la Cuenta de Almacenamiento creada"
+}
+
+output "ip_privada_maquina_virtual" {
+  value       = module.infraestructura_azure.ip_privada_maquina_virtual
+  description = "Dirección IP privada de la Máquina Virtual"
+}
+
+output "dns_maquina_virtual" {
+  value       = module.infraestructura_azure.dns_maquina_virtual
+  description = "DNS de la Máquina Virtual"
+}
